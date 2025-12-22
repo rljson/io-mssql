c72e3ca839c0d82563f7047f9d6000469169202b
Jan <154504062+Jan-Balzer@users.noreply.github.com>
2025-12-22 09:42:18 +0100

    publish correctly (#14)
    * after build
    
    * Increase version

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
-export * from './io-mssql.ts';
+export { adminCfg } from './admin-cfg.ts';
+export { DbBasics } from './db-basics.ts';
+export { IoMssql } from './io-mssql.ts';
